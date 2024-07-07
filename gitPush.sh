ce40e86fe83852d96d7e177581e7a2586d17643b
Konstantin <yarohno@gmail.com>
2024-07-07 19:29:30 +0300

    first_commit

---
+++ gitPush.sh
@@ -0,0 +1,15 @@
+# Добавление удаленных репозиториев
+git remote add origin git@github.com:Koatia/03_Final_Test.git;
+git remote add mirror ssh://git@gitverse.ru:2222/Kostia/03_Final_Test.git
+
+# Пуш изменений в оба репозитория
+git push origin master; git push mirror master
+
+echo '--------'
+
+git status
+
+echo '--------'
+
+echo "Обновление завершено, нажмите ENTER"
+read tem
