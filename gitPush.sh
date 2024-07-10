50ea511243ceb80a4b196281846b79d00576ed01
Konstantin <yarohno@gmail.com>
2024-07-10 21:23:51 +0300

    задание

--- gitPush.sh
+++ gitPush.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Добавление удаленных репозиториев
 git remote add origin git@github.com:Koatia/03_Final_Test.git;
 git remote add mirror ssh://git@gitverse.ru:2222/Kostia/03_Final_Test.git
